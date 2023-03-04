4130fefaf69cba10282f8a534a18915d64818c45
Bruno Futema <bruno.futema@outlook.com>
2023-03-04 00:05:24 -0300

    config: :wrench: add ddd-modular-pattern config

---
+++ backend/ddd-modular-pattern/babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@types': './src/types',
+        '@config': './src/config',
+        '@constants': './src/constants',
+        '@docs': './src/docs',
+        '@jobs': './src/jobs',
+        '@keys': './src/keys',
+        '@modules': './src/modules',
+        '@shared': './src/shared'
+      }
+    }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
+  ],
+  ignore: [
+    '**/*.spec.ts',
+    '**/*.sh'
+  ]
+};
